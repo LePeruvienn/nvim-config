1cb1d337943fb2515f39a2bf4f87d73b0dd912af
LePeruvienn <koto91@free.fr>
2026-04-29 23:55:52 +0200

    Added nvim dap

---
+++ lua/plugins/nvim-dap.lua
@@ -0,0 +1,75 @@
+return {
+	"mfussenegger/nvim-dap",
+	event = "VeryLazy",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"nvim-neotest/nvim-nio",
+		"jay-babu/mason-nvim-dap.nvim",
+		"theHamsta/nvim-dap-virtual-text",
+	},
+	config = function ()
+
+		local mason_dap = require("mason-nvim-dap")
+		local dap = require("dap")
+		local ui = require("dapui")
+		local dap_virtual_text = require("nvim-dap-virtual-text")
+
+		-- Dap Virtual Text
+		dap_virtual_text.setup()
+
+		mason_dap.setup({
+			ensure_installed = { "cppdbg" },
+			automatic_installation = true,
+			handlers = {
+				function(config)
+					require("mason-nvim-dap").default_setup(config)
+				end,
+			},
+		})
+
+		-- Configurations
+		dap.configurations.c = {
+			{
+				name = "Launch file",
+				type = "cppdbg",
+				request = "launch",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+				cwd = "${workspaceFolder}",
+				stopAtEntry = true,
+				MIMode = "gdb",
+			},
+		}
+
+		-- Dap UI
+
+		ui.setup()
+
+		vim.fn.sign_define("DapBreakpoint", { text = "🔴" })
+
+		dap.listeners.before.attach.dapui_config = function()
+			ui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			ui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			ui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			ui.close()
+		end
+
+		-- keymaps
+		local keymap = vim.keymap.set
+
+		keymap("n", "<F5>", dap.continue)
+		keymap("n", "<F10>", dap.step_over)
+		keymap("n", "<F11>", dap.step_into)
+		keymap("n", "<F12>", dap.step_out)
+
+		keymap("n", "<Leader>b", dap.toggle_breakpoint)
+		keymap("n", "<Leader>du", ui.toggle)
+	end,
+}
